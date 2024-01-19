4074de4ba4a398d39376317b1086f80a0141d289
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-19 12:00:26 -0300

    Add config.

---
+++ config/yiisoft-aliases.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiisoft/aliases' => [
+        'aliases' => [
+            '@fontawesome-free' => '@npm/fortawesome--fontawesome-free',
+        ],
+    ],
+];
